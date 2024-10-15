0aac00c2953ad4422fd1726beeeb3797d2c29054
rabar <rabar@LAPTOP-P95BL9K8>
2024-10-15 13:34:09 -0700

    Add project files.

---
+++ Polis oc tjuv test2/Fängelse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polis_oc_tjuv_1
+{
+    public class Fängelse
+    {
+        public List<Tjuv> Fångar { get; private set; }
+
+        public Fängelse()
+        {
+            Fångar = new List<Tjuv>();
+        }
+
+        public void LäggTillFånge(Tjuv tjuv)
+        {
+            Fångar.Add(tjuv);
+            Console.WriteLine("Tjuven har placerats i fängelse.");
+        }
+    }
+}
